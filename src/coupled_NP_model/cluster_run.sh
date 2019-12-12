b1fb0db838234a41724390f52579fd12baa312fc
Benoit Pasquier <pasquieb@uci.edu>
2019-12-12 15:24:36 -0800

    ignore cluster ouput and fix sbatch script by John Yu

--- src/coupled_NP_model/cluster_run.sh
+++ src/coupled_NP_model/cluster_run.sh
@@ -8,12 +8,14 @@
 #SBATCH --mem=32GB
 
 # Load the julia module
+export PATH=~/Applications/julia-1.3.0/bin:$PATH
+export LD_LIBRARY_PATH=~/Applications/julia-1.3.0/lib
 
 # Cd to the root folder on USC HPC cluster
 cd /home/geovault-06/pasquier/Projects/NPM
 
 # Set DataDeps environment variable to download without asking
-DATADEPS_ALWAYS_ACCEPT = true
+DATADEPS_ALWAYS_ACCEPT=true
 
 # Run it!
 julia src/coupled_NP_model/cluster_run.jl
