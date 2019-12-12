b12c1775f3af2979c0541b77ac8ae83eb045036a
Benoit Pasquier <pasquieb@uci.edu>
2019-12-13 10:37:56 +1100

    Add plot at HOT and BATS

---
+++ src/plotting/NvsP_HOTBATS.jl
@@ -0,0 +1,12 @@
+
+using Plots
+
+pyplot()
+plt = ratioatstation(1e3PO₄,1e3DIN,grd,21.0,-158, label="HOT", marker=:diamond, color=:black, ms=6, zlims=(0,1000))
+ratioatstation!(1e3PO₄,1e3DIN,grd,31.5,-64, label="BATS", marker=:square, color=:lightgray, ms=6, zlims=(0,1000))
+xlabel!("PO₄ (μM)", xtickfontsize=14, xguidefontsize=14, xlim=(0,1))
+ylabel!("DIN (μM)", ytickfontsize=14, yguidefontsize=14, ylim=(0,20), yticks=0:2:20)
+plot!(legend=:topleft, legendfontsize=14)
+
+#plot!(plt, dpi=300)
+savefig(plt, "figs/NvsP_upper1000m_coupled_model_notoptimized.pdf")
